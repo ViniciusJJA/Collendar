8df80921e088dabb5e85aa2ee0c2a899240f8ea3
henrique-castanheiro <henrique.castanheirotb@gmail.com>
2025-10-21 20:07:14 -0300

    ...

---
+++ src/test/java/projeto/collendar/CollendarApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.collendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
